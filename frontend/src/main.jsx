0f8ba882f73df4408ad398e9da4e1ed1b17a0812
Soham Chouhan <schouha@ncsu.edu>
2026-06-10 20:03:03 -0400

    trying to configure authentication v2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import "./amplifyConfig"; // Amplify configuration must be imported before any Amplify calls
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
